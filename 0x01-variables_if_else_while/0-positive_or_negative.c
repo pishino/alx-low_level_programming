29b871efdea79619e4892905cc048624bf415bd6
pishino <pishinoforsee@gmail.com>
2022-12-12 12:42:08 -0800

    Another Commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <time.h>
 
 /** 
- * main - main function to generate a random number
- *
- * Return: Always 0 (Success)
+* main - main function to generate a random number
+*
+* Return: Always 0 (Success)
 */
 
 int main(void)
@@ -16,16 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero\n", n);
-	}
 	else if (n < 0)
-	{
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
