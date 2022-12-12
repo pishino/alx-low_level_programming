7c0359cb277ee06048d92ad1515b51ffa71cbfa2
pishino <pishinoforsee@gmail.com>
2022-12-12 12:58:39 -0800

    Betty is straight

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,8 @@
 #include <time.h>
 
 /** 
-* main - main function to generate a random number
-*
-* Return: Always 0 (Success)
+ *main - main function to generate a random number
+ *Return: Always 0 (Success)
 */
 
 int main(void)
@@ -14,7 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
